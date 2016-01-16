92212c89ab758c5f1f7e36b69d13e77dff7493cb
artyom-yurin <artem_yrin@mail.ru>
2016-01-16 21:59:29 +0300

    Fix variables' names
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

--- Wave-algorithm/Labirint_key.pas
+++ Wave-algorithm/Labirint_key.pas
@@ -1,276 +1,270 @@
 PROGRAM Way(INPUT, OUTPUT);
 CONST S = 1001;
 VAR 
-  StartX, StartY, FinishX, FinishY, Y, X, BorderX, BorderY, CheckRoad, Steps: INTEGER;
-  A: ARRAY [0 .. S, 0 .. S] OF INTEGER; {Iannea aey ia?aaioee}
-  B: ARRAY [0 .. S, 0 .. S] OF CHAR; {Iannea aey auaiaa}
+  StartX, StartY, FinishX, FinishY, WindowY, WindowX, SizeX, SizeY, Status, Steps: INTEGER;
+  Digit: ARRAY [0 .. S, 0 .. S] OF INTEGER; {Array for processing}
+  Symbol: ARRAY [0 .. S, 0 .. S] OF CHAR; {Array for save results}
 
 PROCEDURE CheckSize;
 VAR
   F: TEXT;
-BEGIN
+BEGIN {WAY}
   ASSIGN(F, 'LABIRINT.TXT');
   RESET(F);
-  READ(F, B[0, 0]);
-  WHILE (B[0, 0] = '#') AND (NOT EOLN(F))
+  READ(F, Symbol[0, 0]);{INSTAL SizeX}
+  WHILE (Symbol[0, 0] = '#') AND (NOT EOLN(F))
   DO
     BEGIN
-      READ(F, B[0, 0]);
-      IF B[0, 0] = '#'
+      READ(F, Symbol[0, 0]);
+      IF Symbol[0, 0] = '#'
       THEN
-        INC(BorderX)
-    END; 
-  B[0, 0] := '#';
+        INC(SizeX)
+    END;{INSTAL SizeX} 
+  Symbol[0, 0] := '#';{INSTAL SizeY}
   READLN(F);
-  WHILE (B[0, 0] = '#') AND (NOT EOF(F))
+  WHILE (Symbol[0, 0] = '#') AND (NOT EOF(F))
   DO
     BEGIN
-    READ(F, B[0, 0]);
-    READLN(F);
-    IF B[0, 0] = '#'
-    THEN
-      INC(BorderY)  
-    END; 
+      READ(F, Symbol[0, 0]);
+      READLN(F);
+      IF Symbol[0, 0] = '#'
+      THEN
+        INC(SizeY)  
+    END;{INSTAL SizeY} 
   CLOSE(F)  
 END;
-PROCEDURE Reading; {?oaiea}
+PROCEDURE Reading; {READING}
 VAR
   F2: TEXT;                                                   
-  SaveX: INTEGER;
-  Ch: CHAR;
-BEGIN {READING}
+BEGIN {READ FILE}
   ASSIGN(F2, 'LABIRINT.TXT');
   RESET(F2);   
   StartY := 0;
   StartX := 0;
-  Y := 0;
+  WindowY := 0;
   WHILE NOT EOF(F2)
   DO
     BEGIN
-      X := 0;
+      WindowX := 0;
       WHILE NOT EOLN(F2)
       DO
         BEGIN
-          READ(F2, B[X, Y]);
-          IF (B[X, Y] = '*') {Iacia?aiea ia?aeuiiai e eiia?iiai cia?aiey}
+          READ(F2, Symbol[WindowX, WindowY]);
+          IF (Symbol[WindowX, WindowY] = '*') {Specify start and finish}
           THEN
             BEGIN
-              IF (StartY = 0) AND (StartX = 0) {Ia?aeuiia cia?aiea}
+              IF (StartY = 0) AND (StartX = 0) {Specify start}
               THEN
                 BEGIN
-                  StartX := X;
-                  StartY := Y;
-                  A[StartX, StartY] := 1;
-                  FinishX := X;
-                  FinishY := Y;
-                  B[StartX, StartY] := 'O'
-                END {Ia?aeuiia cia?aiea}
+                  StartX := WindowX;
+                  StartY := WindowY;
+                  Digit[StartX, StartY] := 1;
+                  FinishX := WindowX;
+                  FinishY := WindowY;
+                  Symbol[StartX, StartY] := 'O'
+                END {Specify start}
               ELSE
-                IF (FinishX = StartX) AND (FinishY = StartY)
+                IF (FinishX = StartX) AND (FinishY = StartY) {Specify finish}
                 THEN
-                  BEGIN {Eiia?iia cia?aiea}
-                    FinishX := X;
-                    FinishY := Y;
-                    B[FinishX, FinishY] := 'O'
-                  END {Eiia?iia cia?aiea}
+                  BEGIN 
+                    FinishX := WindowX;
+                    FinishY := WindowY;
+                    Symbol[FinishX, FinishY] := 'O'
+                  END {Specify finish}
                 ELSE
-                  CheckRoad := 3;  
-            END; {Iacia?aiea ia?aeuiiai e eiia?iiai cia?aiey}         
-          IF B[X, Y] = '#' {i?iaa?ea ia noaiee} 
+                  Status := 3;{ERROR}  
+            END; {Specify start and finish}         
+          IF Symbol[WindowX, WindowY] = '#' {Indicates walls} 
           THEN
-            A[X, Y] := -1
-          ELSE {i?iaa?ea ia noaiee}
-            IF B[X, Y] = ' ' {i?iaa?ea ia ionoia ianoi}
+            Digit[WindowX, WindowY] := -1
+          ELSE {Indicates walls}
+            IF Symbol[WindowX, WindowY] = ' ' {Indicates spaces}
             THEN
-              A[X, Y] := 0; {i?iaa?ea ia ionoia ianoi}
-          IF ((X > BorderX) OR (Y > BorderY)) AND (B[X, Y] <> ' ')
+              Digit[WindowX, WindowY] := 0; {Indicates spaces}
+          IF ((WindowX > SizeX) OR (WindowY > SizeY)) AND (Symbol[WindowX, WindowY] <> ' '){CHECK FRAME}
           THEN
-            CheckRoad := 3;
-          IF Y = BorderY
+            Status := 3;{CHECK FRAME}
+          IF WindowY = SizeY {Check INTEGRITY BOX IN WindowX}
           THEN
-            IF (B[X, BorderY] <> '#') AND (X <= BorderX)
+            IF (Symbol[WindowX, SizeY] <> '#') AND (WindowX <= SizeX)
             THEN
-              CheckRoad := 3;      
-          X := X + 1;    
+              Status := 3;{Check INTEGRITY BOX IN WindowX}      
+          INC(WindowX)    
         END;  
       READLN(F2);
-      IF (B[BorderX, Y] <> '#') AND (Y <= BorderY)
+      IF (Symbol[SizeX, WindowY] <> '#') AND (WindowY <= SizeY) {Check INTEGRITY BOX IN WindowY}
       THEN
-        CheckRoad := 3;  
-      Y := Y + 1
-    END; {READ}
+        Status := 3;{Check INTEGRITY BOX IN WindowX}  
+      INC(WindowY)
+    END; {READ FILE}
     IF ((StartX = 0) AND (StartY = 0)) OR ((FinishX = StartX) AND (FinishY = StartY))
     THEN
-      CheckRoad := 3;
+      Status := 3;
   CLOSE(F2); 
 END; {READING}
 
-FUNCTION WAVES(CountSteps: INTEGER): INTEGER;
+FUNCTION LetWaves(CountSteps: INTEGER): INTEGER;
 BEGIN {WAVES}
-  IF CheckRoad <> 3 
+  IF Status <> 3 
   THEN
-    CheckRoad := 1;
-  WHILE (A[FinishX, FinishY] = 0) AND (CheckRoad = 1) {WAVES}
+    Status := 1;
+  WHILE (Digit[FinishX, FinishY] = 0) AND (Status = 1) {WAVES}
   DO
     BEGIN
-      CheckRoad := 0; {I?iaa?ea ia caiieiaiinou}
-      FOR Y := 0 TO BorderY 
+      Status := 0; {Check for occupancy}
+      FOR WindowY := 0 TO SizeY 
       DO
         BEGIN
-          FOR X := 0 TO BorderX
+          FOR WindowX := 0 TO SizeX
           DO
             BEGIN
-              IF A[X, Y] = CountSteps - 1
+              IF Digit[WindowX, WindowY] = CountSteps - 1
               THEN
                 BEGIN
-                  IF A[X + 1, Y] = 0 {?anni?ino?iiaiea aieiu}
+                  IF Digit[WindowX + 1, WindowY] = 0 {Wave propagation}
                   THEN
                     BEGIN                
-                      A[X + 1, Y] := CountSteps; {Ai?aai}
-                      CheckRoad := 1
+                      Digit[WindowX + 1, WindowY] := CountSteps; {Right}
+                      Status := 1
                     END;
-                  IF A[X - 1, Y] = 0
+                  IF Digit[WindowX - 1, WindowY] = 0
                   THEN
                     BEGIN
-                      A[X - 1, Y] := CountSteps; {Aeaai}
-                      CheckRoad := 1
+                      Digit[WindowX - 1, WindowY] := CountSteps; {Left}
+                      Status := 1
                     END;
-                  IF A[X, Y + 1] = 0
+                  IF Digit[WindowX, WindowY + 1] = 0
                   THEN
                     BEGIN
-                      A[X, Y + 1] := CountSteps; {Aiec}
-                      CheckRoad := 1
+                      Digit[WindowX, WindowY + 1] := CountSteps; {Down}
+                      Status := 1
                     END;
-                  IF A[X, Y - 1] = 0
+                  IF Digit[WindowX, WindowY - 1] = 0
                   THEN
                     BEGIN
-                      A[X, Y - 1] := CountSteps; {Aaa?o}
-                      CheckRoad := 1
-                    END {?anni?ino?iiaiea aieiu} 
+                      Digit[WindowX, WindowY - 1] := CountSteps; {Up}
+                      Status := 1
+                    END {Wave propagation} 
                 END  
             END
         END;
-      CountSteps := CountSteps + 1 {N?ao?ee eooa?aoee}  
+      INC(CountSteps) {Count Steps}  
     END; {WAVES}
-  CountSteps := CountSteps - 2; {Au?eoaai aae?aiea a ia?aea e a eiioa}
-  Steps := CountSteps; {Nio?aiaiea iooe}
+  CountSteps := CountSteps - 2; {Subtract movement at the beginning and at the end}
+  Steps := CountSteps; {Save resault}
   RETURN(CountSteps)
 END; {WAVES}
  
-FUNCTION WAY: INTEGER;
+PROCEDURE SearchWay;
 VAR
-  CountSteps, SX, SY: INTEGER;
+  CountSteps, WayX, WayY: INTEGER;
 BEGIN {WAY}
-  CountSteps := WAVES(2);
-  IF CheckRoad = 1 {I?iaa?ea ia iaee?ea iooe}
+  CountSteps := LetWaves(2);
+  IF Status = 1 {Checking for path}
   THEN
     BEGIN
       IF (StartX <> FinishX) OR (StartY <> FinishY)
       THEN
         BEGIN
-          IF A[FinishX + 1, FinishY] = CountSteps {I?iaa?yi eeaoee ?yaii e caaaai ia?ao? eii?aeiaoo}
+          IF Digit[FinishX + 1, FinishY] = CountSteps {Check the box next and sets the first coordinate}
           THEN
             BEGIN
-              B[FinishX + 1, FinishY] := '*'; {i?iaa?ea Ni?aaa}
-              SX := FinishX + 1; {Caaaiea eii?aeiao ia?aea iooe}
-              SY := FinishY 
+              Symbol[FinishX + 1, FinishY] := '*'; {Check Right}
+              WayX := FinishX + 1; {Setting coordinate beginning of the path}
+              WayY := FinishY 
             END
           ELSE  
-            IF A[FinishX - 1, FinishY] = CountSteps 
+            IF Digit[FinishX - 1, FinishY] = CountSteps 
             THEN
               BEGIN
-                B[FinishX - 1, FinishY] := '*'; {i?iaa?ea Neaaa}
-                SX := FinishX - 1; {Caaaiea eii?aeiao ia?aea iooe}
-                SY := FinishY 
+                Symbol[FinishX - 1, FinishY] := '*'; {Check Left}
+                WayX := FinishX - 1; {Setting coordinate beginning of the path}
+                WayY := FinishY 
               END
             ELSE
-              IF A[FinishX, FinishY + 1] = CountSteps 
+              IF Digit[FinishX, FinishY + 1] = CountSteps 
               THEN
                 BEGIN
-                  B[FinishX, FinishY + 1] := '*'; {i?iaa?ea Nieco}
-                  SX := FinishX; {Caaaiea eii?aeiao ia?aea iooe}
-                  SY := FinishY + 1
+                  Symbol[FinishX, FinishY + 1] := '*'; {Check Down}
+                  WayX := FinishX; {Setting coordinate beginning of the path}
+                  WayY := FinishY + 1
                 END
               ELSE
-                IF A[FinishX, FinishY - 1] = CountSteps 
+                IF Digit[FinishX, FinishY - 1] = CountSteps 
                 THEN
                   BEGIN
-                    B[FinishX, FinishY - 1] := '*'; {i?iaa?ea Naa?oo}
-                    SX := FinishX; {Caaaiea eii?aeiao ia?aea iooe}
-                    SY := FinishY - 1
+                    Symbol[FinishX, FinishY - 1] := '*'; {Check Up}
+                    WayX := FinishX; {Setting coordinate beginning of the path}
+                    WayY := FinishY - 1
                   END;
-            WHILE (B[StartX, StartY] = 'O') AND (CountSteps >= 0)
+            WHILE (Symbol[StartX, StartY] = 'O') AND (CountSteps >= 0)
             DO
               BEGIN
                 CountSteps := CountSteps - 1;
-                IF A[SX + 1, SY] = CountSteps {I?iaa?yi eeaoee ?yaii e auno?aeaaai ioou}
+                IF Digit[WayX + 1, WayY] = CountSteps {Check the box next and build the way}
                 THEN
                   BEGIN
-                    B[SX + 1, SY] := '*'; {I?iaa?yi Ni?aaa}
-                    SX := SX + 1
+                    Symbol[WayX + 1, WayY] := '*'; {Check Right}
+                    INC(WayX)
                   END
                 ELSE  
-                  IF A[SX - 1, SY] = CountSteps 
+                  IF Digit[WayX - 1, WayY] = CountSteps 
                   THEN
                     BEGIN
-                      B[SX - 1, SY] := '*'; {I?iaa?yi Neaaa}
-                      SX := SX - 1
+                      Symbol[WayX - 1, WayY] := '*'; {Check Left}
+                      DEC(WayX)
                     END
                   ELSE
-                    IF A[SX, SY + 1] = CountSteps 
+                    IF Digit[WayX, WayY + 1] = CountSteps 
                     THEN
                       BEGIN
-                        B[SX, SY + 1] := '*'; {I?iaa?yi Nieco}
-                        SY := SY + 1
+                        Symbol[WayX, WayY + 1] := '*'; {Check Down}
+                        INC(WayY)
                       END
                     ELSE
-                      IF A[SX, SY - 1] = CountSteps 
+                      IF Digit[WayX, WayY - 1] = CountSteps 
                       THEN
                         BEGIN
-                          B[SX, SY - 1] := '*'; {I?iaa?yi Naa?oo}
-                          SY := SY - 1
+                          Symbol[WayX, WayY - 1] := '*'; {Check Up}
+                          DEC(WayY)
                         END;     
               END
         END      
-    END;
-  RETURN 0    
+    END;   
 END;{WAY} 
 
-FUNCTION Print(): INTEGER;
+PROCEDURE Print;
 VAR
   F1: TEXT;
 BEGIN
   ASSIGN(F1, 'LABIRINT_EXIT.TXT');
   REWRITE(F1);
-  B[StartX, StartY] := 'O'; 
-  IF CheckRoad = 1 
+  Symbol[StartX, StartY] := 'O'; 
+  IF Status = 1 
   THEN
-    WRITELN(F1, 'Shortest way is ', Steps) {Anee ioou eiaaony}
+    WRITELN(F1, 'Shortest way is ', Steps) {If there is Digit the way}
   ELSE
-    IF CheckRoad = 0
+    IF Status = 0
     THEN
-      WRITELN(F1, 'There is no way!!!') {Anee e eiioo iao ai?ae} 
+      WRITELN(F1, 'There is no way!!!') {If at the end there is no way} 
     ELSE
-      WRITE(F1, 'ERROR');
-  IF CheckRoad <> 3 
+      WRITE(F1, 'ERROR'); {If the data is not correct}
+  IF Status <> 3 
   THEN      
-    FOR Y := 0 TO BorderY {Ia?ou ?acoeuoaoa}
+    FOR WindowY := 0 TO SizeY {Print Result}
     DO
       BEGIN
-        FOR X := 0 TO BorderX
-        DO
-          BEGIN             
-            WRITE(F1, B[X, Y])  
-          END;
+        FOR WindowX := 0 TO SizeX
+        DO             
+          WRITE(F1, Symbol[WindowX, WindowY]);  
         WRITELN(F1) 
-      END; {Ia?ou ?acoeuoaoa}
-  CLOSE(F1);
-  RETURN 0     
+      END; {Print Result}
+  CLOSE(F1)     
 END;
  
 BEGIN
   CheckSize;
-  Reading();{?OAIEA}
-  Way();{IINO?IAIIEA IEIEIAEUIIAI IOOE}
-  Print(){AUAIA ?ACOEUOAOA}
-END.
+  Reading;
+  SearchWay;
+  Print
+END. {WAY}
